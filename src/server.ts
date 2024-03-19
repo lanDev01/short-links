03aaac7c0c1242352ea2e7e023fcba1b5d2e1008
LanDev01 <alnjr0001@outlook.com>
2024-03-19 15:37:10 -0300

    feat: Criando rota de metricas e tratando retorno

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import { z } from "zod";
 import { sql } from "./lib/postgres";
 import postgres from "postgres";
+import { redis } from "./lib/redis";
 
 const app = fastify()
 
@@ -25,6 +26,8 @@ app.get('/:code', async (request, reply) => {
     }
 
     const link = result[0]
+
+    await redis.zIncrBy('metrics', 1, String(link.id))
 
     return reply.redirect(301, link.original_url)
 })
@@ -71,6 +74,21 @@ app.post('/api/links', async (request, reply) => {
             message: 'Internal Server Error'
         })
     }
+})
+
+app.get('/api/metrics', async () => {
+    const result = await redis.zRangeByScoreWithScores('metrics', 0, 50)
+
+    const metrics = result
+        .sort((a, b) => a.score - b.score)
+        .map(item => {
+            return {
+                shortLinkId: Number(item.value),
+                clicks: item.score,
+            }
+        })
+
+    return metrics
 })
 
 app.listen({
